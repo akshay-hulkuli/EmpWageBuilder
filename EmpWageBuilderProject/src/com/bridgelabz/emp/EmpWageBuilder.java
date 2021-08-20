2903333f2cd8302aa02bc8823bf27ffcbb219dab
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 12:35:46 +0530

    MODIFIED Based on UC2

--- EmpWageBuilderProject/src/com/bridgelabz/emp/EmpWageBuilder.java
+++ EmpWageBuilderProject/src/com/bridgelabz/emp/EmpWageBuilder.java
@@ -4,14 +4,20 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		//Constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		//variables
+		int empHrs = 0;
+		int empWage = 0;
 		//compuation
 		double empCheck = Math.floor(Math.random()*10) % 2;
 		if( empCheck == IS_FULL_TIME) {
-			System.out.println(" Employee is present");
+			empHrs = 8;
 		}
 		else {
-			System.out.println("Employee is absent");
+			empWage = 0;
 		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage : "+ empWage);
 	}
 
 }
