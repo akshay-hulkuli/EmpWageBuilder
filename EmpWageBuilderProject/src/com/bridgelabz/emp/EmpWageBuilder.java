6e5036bd0e7e531eb7bf0706bbf190678a54f3a3
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 12:07:48 +0530

    Basic Java Project

---
+++ EmpWageBuilderProject/src/com/bridgelabz/emp/EmpWageBuilder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.emp;
+
+public class EmpWageBuilder {
+	public static void main(String[] args) {
+		System.out.println("**** Welcom to Employee Wage Computaion program ****");
+	}
+
+}
