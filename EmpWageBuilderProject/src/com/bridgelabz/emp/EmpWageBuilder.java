68c0e29c6b4f62c8e8325fb2d125a33395e3a000
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-26 20:57:57 +0530

    ADDED:Class method to compute wage

--- EmpWageBuilderProject/src/com/bridgelabz/emp/EmpWageBuilder.java
+++ EmpWageBuilderProject/src/com/bridgelabz/emp/EmpWageBuilder.java
@@ -8,7 +8,7 @@ public class EmpWageBuilder {
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HOURS_IN_MONTH = 100;
 	
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		//variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays =0;
 		//compuation
@@ -16,20 +16,25 @@ public class EmpWageBuilder {
 			totalWorkingDays ++;
 			int empCheck = (int) Math.floor(Math.random()*10) % 3;
 			switch (empCheck) {
-			case IS_PART_TIME : 
-				empHrs = 4;
-				break;
-			case IS_FULL_TIME :
-				empHrs = 8;
-				break;
-			default :
-				empHrs = 0;
+				case IS_PART_TIME : 
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME :
+					empHrs = 8;
+					break;
+				default :
+					empHrs = 0;
 			}
 		
 			totalEmpHrs += empHrs;
 			System.out.println("Days#: "+totalWorkingDays+"     Emp Hrs: "+ empHrs);
 		}
 		int totalWage = totalEmpHrs *totalWorkingDays;
+		return totalWage;
+	}
+	
+	public static void main(String[] args) {
+		int totalWage = computeEmpWage();
 		System.out.println("Total Emp Wage: " + totalWage );
 	}
 
