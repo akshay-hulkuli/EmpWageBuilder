d5316b7b8ebb71d3d869f15003cac91a630d0fec
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-27 11:10:35 +0530

    ADDED:ability to save wage for each company

--- EmpWageBuilderProject/src/com/bridgelabz/emp/EmpWageBuilder.java
+++ EmpWageBuilderProject/src/com/bridgelabz/emp/EmpWageBuilder.java
@@ -5,10 +5,11 @@ public class EmpWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	
-	private static String company;
-	private static int empRatePerHour;
-	private static int numOfWorkingDays;
-	private static int maxHoursInMonth ;
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursInMonth ;
+	private int totalWage;
 	
 	EmpWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth){
 		this.company = company;
@@ -21,7 +22,7 @@ public class EmpWageBuilder {
 
 		//variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays =0;
-		//compuation
+		//computation
 		while(totalEmpHrs <= maxHoursInMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays ++;
 			int empCheck = (int) Math.floor(Math.random()*10) % 3;
@@ -39,16 +40,24 @@ public class EmpWageBuilder {
 			totalEmpHrs += empHrs;
 			System.out.println("Days#: "+totalWorkingDays+"     Emp Hrs: "+ empHrs);
 		}
-		int totalWage = totalEmpHrs *empRatePerHour;
-		System.out.println("The total wage of employee in "+company+" is : "+ totalWage);
+		totalWage = totalEmpHrs *empRatePerHour;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "The total wage of employee in "+company+" is : "+ totalWage;
 	}
 	
 	public static void main(String[] args) {
 		EmpWageBuilder dMart = new EmpWageBuilder("Dmart",20, 5, 50);
-		dMart.computeEmpWage();
-		
 		EmpWageBuilder Reliance = new EmpWageBuilder("Reliance",22, 6, 60);
+		
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		
 		Reliance.computeEmpWage();
+		System.out.println(Reliance);
 		
 	}
 
